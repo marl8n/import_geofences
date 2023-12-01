ea3939830972d090fde6d049a7b165d671a06252
Marlon De León <ivandmarlon@gmail.com>
2023-12-01 15:59:38 -0600

    Save files

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-import json
-import re
-from typing import Dict, List
-
+from csv_parser.parse_csv_to_json import parse_csv_to_json
+from writer.write_json import write
+from os import listdir
 
 def run():
-    pattern = re.compile(r'^(\w+\s?\(.*\)),([\w|\s\-ÁÉÍÓÚ]*),([\w|\s\-ÁÉÍÓÚ]*),.*$')
-    parsed: List[Dict[str, str]] = []
-    with open('./geofences_csv/Lotes -Torre de control- EX-EMILIAS.csv', 'r') as f:
-        lines = f.readlines()
-        print("Begin process")
-        for line in lines:
-            result = re.match(pattern, line.strip())
+    base_csv_dir = './geofences_csv'
+    filenames = [f for f in listdir(base_csv_dir)]
 
-            if not result: continue
-
-            wkt = result.group(1)
-            name = result.group(2)
-            description = result.group(3)
-
-            parsed.append({
-                "wkt": wkt,
-                "name": name,
-                "description": description
-            })
-
-        print("End process")
-
-    new_file = open('01_Lotes-Torre_de_control-EX-EMILIAS.json', 'w')
-    new_file.write(json.dumps(parsed))
-    new_file.close()
+    for f in filenames:
+        parsed = parse_csv_to_json(f'{base_csv_dir}/{f}')
+        formatted_name = f.replace('.csv', '')
+        write(parsed, f'{formatted_name}.json')
 
 if __name__ == '__main__':
     run()
