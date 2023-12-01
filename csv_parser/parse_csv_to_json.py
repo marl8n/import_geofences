ecec153a533e6e0a4d4bb6ec2cca5530b37395c3
Marlon De León <ivandmarlon@gmail.com>
2023-12-01 15:55:15 -0600

    removed unusefule validation

--- csv_parser/parse_csv_to_json.py
+++ csv_parser/parse_csv_to_json.py
@@ -1,14 +1,8 @@
 import json
 import re
 from typing import Dict, List
-from os.path import is_file
 
-def validate(filename: str):
-    if not is_file(filename): raise NameError(f'{filename}: is not a valid file')
-
-def parse(filename: str) -> str:
-    validate(filename)
-
+def parse_csv_to_json(filename: str) -> str:
     pattern = re.compile(r'^(\w+\s?\(.*\)),([\w|\s\-ÁÉÍÓÚ]*),([\w|\s\-ÁÉÍÓÚ]*),.*$')
     parsed: List[Dict[str, str]] = []
     # ./geofences_csv/Lotes -Torre de control- EX-EMILIAS.csv
