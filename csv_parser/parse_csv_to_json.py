df12d35910353345fb0c93be2f6d4858f065ad8f
Marlon De León <ivandmarlon@gmail.com>
2023-12-02 08:01:51 -0600

    implement logic for pattern and rowId

--- csv_parser/parse_csv_to_json.py
+++ csv_parser/parse_csv_to_json.py
@@ -3,29 +3,40 @@ import re
 from typing import Dict, List
 
 def parse_csv_to_json(filename: str) -> str:
-    if filename.endswith('Lotes -Torre de control- EX-EMILIAS.csv'): return ""
-    # first group KWT
-    # second group name
-    # third group description
-    pattern = re.compile(r'^(".*"),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
     parsed: List[Dict[str, str]] = []
+    pattern = None
+
+    if not filename.endswith('Lotes -Torre de control- EX-EMILIAS.csv'):
+        # first group KWT
+        # second group name
+        # third group description
+        # with ""
+        pattern = re.compile(r'^(".*"),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
+    else:
+        # without ""
+        pattern = re.compile(r'^([A-Z]+\s\({1,2}.*\){1,2}),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
+
+    if not pattern: return ""
+
     # ./geofences_csv/Lotes -Torre de control- EX-EMILIAS.csv
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8-sig' ) as f:
         lines = f.readlines()
         print(f"Begin process: {filename}")
+        idx = 0
         for line in lines:
             result = re.match(pattern, line.strip())
 
             if not result: continue
 
-            wkt = result.group(1)
+            wkt = result.group(1).replace('"', '')
             name = result.group(2)
-            description = result.group(3)
+
+            idx += 1
 
             parsed.append({
+                "rowId": idx,
                 "wkt": wkt,
                 "name": name,
-                "description": description
             })
 
         print("End process")
