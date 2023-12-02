1c00f33e43d1859c56e0935a9a8b056ad3739dca
Marlon De León <ivandmarlon@gmail.com>
2023-12-02 10:53:51 -0600

    Fixed for all files

--- csv_parser/parse_csv_to_json.py
+++ csv_parser/parse_csv_to_json.py
@@ -4,21 +4,8 @@ from typing import Dict, List
 
 def parse_csv_to_json(filename: str) -> str:
     parsed: List[Dict[str, str]] = []
-    pattern = None
+    pattern = re.compile(r'^(".*"),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
 
-    if not filename.endswith('Lotes -Torre de control- EX-EMILIAS.csv'):
-        # first group KWT
-        # second group name
-        # third group description
-        # with ""
-        pattern = re.compile(r'^(".*"),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
-    else:
-        # without ""
-        pattern = re.compile(r'^([A-Z]+\s\({1,2}.*\){1,2}),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
-
-    if not pattern: return ""
-
-    # ./geofences_csv/Lotes -Torre de control- EX-EMILIAS.csv
     with open(filename, 'r', encoding='utf-8-sig' ) as f:
         lines = f.readlines()
         print(f"Begin process: {filename}")
@@ -30,6 +17,9 @@ def parse_csv_to_json(filename: str) -> str:
 
             wkt = result.group(1).replace('"', '')
             name = result.group(2)
+
+            if result.group(3):
+                name = f'{name} - {result.group(3)}'
 
             idx += 1
 
