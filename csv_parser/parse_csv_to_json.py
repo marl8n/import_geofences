481292a51d3fa6c76d7aafcbcb4ec4a889e7672e
Marlon De León <ivandmarlon@gmail.com>
2023-12-05 09:26:09 -0600

    added vertex count

--- csv_parser/parse_csv_to_json.py
+++ csv_parser/parse_csv_to_json.py
@@ -4,35 +4,46 @@ from typing import Dict, List
 
 from geofences_utils.parsers import parse_wkt_to_csv
 
+
 def parse_csv_to_json(filename: str) -> str:
     parsed: List[Dict[str, str]] = []
-    pattern = re.compile(r'^(".*"),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$')
+    pattern = re.compile(
+        r'^(".*"),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),([\w\d\s\.\-º\(\)#áéíóúÁÉÍÓÚñÑ]*),.*$'
+    )
 
-    with open(filename, 'r', encoding='utf-8-sig' ) as f:
+    with open(filename, "r", encoding="utf-8-sig") as f:
         lines = f.readlines()
         print(f"Begin process: {filename}")
         idx = 0
         for line in lines:
             result = re.match(pattern, line.strip())
 
-            if not result: continue
+            if not result:
+                continue
 
-            wkt = result.group(1).replace('"', '')
+            wkt = result.group(1).replace('"', "")
+
             name = result.group(2)
 
-            vertex_csv = parse_wkt_to_csv(wkt)
+            vertex_csv, number_of_coordinates = parse_wkt_to_csv(wkt)
+
+            if number_of_coordinates < 3:
+                continue
 
             if result.group(3):
-                name = f'{name} - {result.group(3)}'
+                name = f"{name} - {result.group(3)}"
 
             idx += 1
 
-            parsed.append({
-                "rowId": idx,
-                "wkt": wkt,
-                "name": name,
-                "vertexCsv": vertex_csv,
-            })
+            parsed.append(
+                {
+                    "rowId": idx,
+                    "wkt": wkt,
+                    "name": name,
+                    "vertexCsv": vertex_csv,
+                    "vertexCount": len(list(vertex_csv.split(";"))),
+                }
+            )
 
         print("End process")
         return json.dumps(parsed)
