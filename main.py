bd9d44a9c8b3168c4ee44ab452eb679bebbaa389
Marlon De León <ivandmarlon@gmail.com>
2023-12-05 09:25:44 -0600

    formatted

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from csv_parser.parse_csv_to_json import parse_csv_to_json
 from writer.write_json import write
 from os import listdir
 
+
 def run():
-    base_csv_dir = './geofences_csv'
+    base_csv_dir = "./geofences_csv"
     filenames = [f for f in listdir(base_csv_dir)]
 
     for f in filenames:
-        parsed = parse_csv_to_json(f'{base_csv_dir}/{f}')
-        formatted_name = f.replace('.csv', '')
-        write(parsed, f'{formatted_name}')
+        parsed = parse_csv_to_json(f"{base_csv_dir}/{f}")
+        formatted_name = f.replace(".csv", "")
+        write(parsed, f"{formatted_name}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
